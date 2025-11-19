39bd1d1be3c2ecccda75e2992ad617ed34e88202
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-11-19 20:44:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,11 +4,16 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Provider Configuration - NDRROJI KËTO ME TË DHËNAT E PROVIDERIT TËND
+// PROVIDER CONFIG - NDRROJI KËTO!
 define('IPTV_PROVIDER_URL', 'http://testdi1.proxytx.cloud:80');
 define('IPTV_USERNAME', 'username_your');
-define('IPTV_PASSWORD', 'password_your');
+define('IPTV_PASSWORD', 'password_your'); 
 define('IPTV_MAC_ADDRESS', '00:1A:79:29:86:BB');
+
+// API Endpoints të providerit
+define('PROVIDER_API_CHANNELS', IPTV_PROVIDER_URL . '/player_api.php');
+define('PROVIDER_API_LIVE_STREAMS', IPTV_PROVIDER_URL . '/live/'. IPTV_USERNAME .'/'. IPTV_PASSWORD .'/');
+define('PROVIDER_API_EPG', IPTV_PROVIDER_URL . '/xmltv.php');
 
 // Application configuration
 define('BASE_URL', 'https://' . ($_SERVER['HTTP_HOST'] ?? 'localhost'));
