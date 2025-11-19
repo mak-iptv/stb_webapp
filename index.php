5908da9a1d6595b32db93971c222df7729c7fc07
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-11-20 00:02:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // index.php
 session_start();
-require_once 'includes/functions.php';
+require_once __DIR__ . '/includes/functions.php';
 
 // Kontrollo nëse është lidhur
 if (!isset($_SESSION['portal_url'])) {
@@ -11,6 +11,13 @@ if (!isset($_SESSION['portal_url'])) {
 
 // Merr kanalet
 $channels = $_SESSION['channels'] ?? getChannelsFromProvider($_SESSION['portal_url']);
+
+// Shfaq informacion për kanalet
+echo "<h1>Kanalet e TV</h1>";
+echo displayChannelsInfo($channels);
+
+// Ose për debug, shfaq response-t e API
+// debugApiResponse($_SESSION['portal_url'], $_SESSION['user_mac'] ?? generateMacAddress());
 
 // Shfaq kanalet
 echo displayChannelsList($channels, $_SESSION['portal_url']);
