4556075c1336381981fbf6e86482465c49e17718
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-11-19 21:55:50 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// STALKER MIDDLEWARE CONFIG - NDRROJI KËTO ME TË DHËNAT E TUJA!
-define('STALKER_PORTAL_URL', 'http://testdi1.proxytx.cloud:80'); // NDRROJE
-define('STALKER_MAC_ADDRESS', '00:1A:79:29:86:BB'); // NDRROJE
-define('STALKER_USERNAME', 'stb'); // NDRROJE  
-define('STALKER_PASSWORD', 'stb'); // NDRROJE
-
+// Nuk kemi më konfigurim të fiksuar - do të merret nga login form
 // Application configuration
 define('BASE_URL', 'https://' . ($_SERVER['HTTP_HOST'] ?? 'localhost'));
 define('CACHE_ENABLED', false);
